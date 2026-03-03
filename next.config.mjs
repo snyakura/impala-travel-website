ca893ed3f686424fdad1de2a48d275debcd35165
snyakura <131558650+snyakura@users.noreply.github.com>
2026-03-03 05:37:33 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export', // Tells Next.js to build a static site
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js image optimization
   },
-}
+  // If your URL is https://username.github.io/repo-name/ 
+  // uncomment the line below and replace 'repo-name' with your actual repo name
+  // basePath: '/impala-travel-website', 
+};
 
-export default nextConfig
+export default nextConfig;
